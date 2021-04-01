ee16f61fa01c03c6578b5f72011bd06aaeea9d89
Taylor Sanders <tsanders3515@gmail.com>
2021-03-31 21:33:19 -0800

    login, logout, sign up handlebars + js

---
+++ public/login.js
@@ -0,0 +1,25 @@
+const loginFormHandler = async (data) => {
+    data.preventDefault();
+
+    //COLLECT VALUES FROM THE LOGIN FORM
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (username && password) {
+        //SEND a POST REQUEST TO THE API ENDPOINT
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            // IF SUCCESSFUL, REDIRECT THE BROWSER TO THE POST PAGE
+            document.location.replace('/post');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
