b3b53494cbb31cfe5c1a793689937d6e1d930ad0
Taylor Sanders <tsanders3515@gmail.com>
2021-04-08 17:15:43 -0800

    finished update.js and edit-post.handlebars

--- public/js/update.js
+++ public/js/update.js
@@ -6,10 +6,19 @@ async function editFormHandler(event) {
     const content = document.querySelector('input[name="content"]').value;
 
     //BELOW - api/post COMING FROM API INDEX.JS
-const response = await fetch(`/api/post`, {
-method: 'POST',
-body: JSON.stringify({
-    post_id
-})
-})
-}
+    const response = await fetch(`/api/post`, {
+        method: 'POST',
+        body: JSON.stringify({ post_id: id, title, content }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+document.querySelector('#edit-form-button').addEventListener('click', editFormHandler);
