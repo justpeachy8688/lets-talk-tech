599a0c8b472db2111b2e2600200cf46e66458753
Taylor Sanders <tsanders3515@gmail.com>
2021-04-13 17:21:44 -0800

    working on update

---
+++ public/js/edit.js
@@ -0,0 +1,40 @@
+const postId = document.querySelector('input[name="post-id"]');
+
+const editFormHandler = async function (event) {
+    event.preventDefault();
+    // GO GRAB WHAT IS INSIDE THE TITLE INPUT AREA
+    const title = document.querySelector('input[name="post-title"]').value;
+    // GO GRAB WHAT IS INSIDE THE CONTENT INPUT AREA
+    const content = document.querySelector('input[name="content"]').value;
+
+    //BELOW - api/post COMING FROM API INDEX.JS
+    const response = await fetch(`/api/post/${postId}`, {
+        method: 'PUT',
+        body: JSON.stringify({ title, content }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+const deleteFormHandler = async function () {
+    const response = await fetch(`/api/post/${postId}`, {
+        method: 'DELETE',
+    });
+    if (response.ok) {
+        location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('#edit-form-button').addEventListener('submit', editFormHandler);
+
+document.querySelector('#delete-button').addEventListener('click', deleteFormHandler);
+
--- public/js/signUp.js
+++ public/js/signUp.js
@@ -17,7 +17,7 @@ const signupFormHandler = async (event) => {
         });
 
         if (response.ok) {
-            document.location.replace('/dashboard');
+            document.location.replace('/homepage');
         } else {
             alert(response.statusText);
         }
